10870bd40239b826bdb4050a102d38832c58e623
SinghM21 <72313996+SinghM21@users.noreply.github.com>
2025-09-09 21:17:04 +0100

    Added initial income + outcome to projection graph

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using FinancePlanner.Contexts;
 using FinancePlanner.Models;
+using FinancePlanner.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
 using System.Diagnostics;
 
 namespace FinancePlanner.Controllers
@@ -8,15 +12,23 @@ namespace FinancePlanner.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly FinancePlannerContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, FinancePlannerContext financePlannerContext)
         {
             _logger = logger;
+            _context = financePlannerContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            DashboardViewModel dashboardViewModel = new DashboardViewModel()
+            {
+                monthlyIncome = _context.Income.Sum(i => i.Amount),
+                monthlyOutcome = _context.Outcome.Sum(o => o.Cost)
+            };
+
+            return View(dashboardViewModel);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FinancePlanner.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int monthlyIncome { get; set; }
+        public int monthlyOutcome { get; set; }
+    }
+}
