113d4dab32cdd889e90f38c5a2691f041cba273b
SinghM21 <72313996+SinghM21@users.noreply.github.com>
2025-09-28 17:47:23 +0100

    Added NotEnumValueAttribute unit test + refactored Unit Tests in Helpers

---
+++ FinancePlannerTests/Attributes/Validation/NotEnumValueAttributeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancePlanner.Attributes.Validation;
+
+namespace FinancePlannerTests.Attributes.Validation
+{
+    public class NotEnumValueAttributeTests
+    {
+        [Fact]
+        public void IsValid_ShouldReturnFalse_WhenValueIsInvalidEnumValue()
+        {
+            // Arrange
+            var attribute = new NotEnumValueAttribute(DayOfWeek.Monday);
+
+            // Act
+            var result = attribute.IsValid(DayOfWeek.Monday);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
