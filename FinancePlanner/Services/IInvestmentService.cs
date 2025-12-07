6f92e158df9ccc434f54d66b968048ac087932f1
SinghM21 <manis3r3@gmail.com>
2025-12-07 18:48:57 +0000

    Init investment service

--- FinancePlanner/Services/IInvestmentService.cs
+++ FinancePlanner/Services/IInvestmentService.cs
@@ -1,8 +1,14 @@
-﻿namespace FinancePlanner.Services
+﻿using FinancePlanner.DTOs;
+
+namespace FinancePlanner.Services
 {
     public interface IInvestmentService
     {
-
+        public Task<InvestmentDto> CreateInvestmentAsync(InvestmentDto investmentDto);
+        public Task<InvestmentDto?> UpdateInvestmentAsync(int id, InvestmentDto investmentDto);
+        public Task<bool> DeleteInvestmentAsync(int id);
+        public Task<InvestmentDto?> GetInvestmentByIdAsync(int id);
+        public Task<IReadOnlyList<InvestmentDto>> GetAllInvestmentsAsync();
 
     }
 }
---
+++ FinancePlanner/Services/InvestmentService.cs
@@ -0,0 +1,51 @@
+﻿using FinancePlanner.Contexts;
+using FinancePlanner.DTOs;
+using FinancePlanner.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancePlanner.Services
+{
+    public class InvestmentService : IInvestmentService
+    {
+        FinancePlannerContext _context;
+        IInvestmentMapper _investmentMapper;
+        public InvestmentService(FinancePlannerContext context, IInvestmentMapper investmentMapper) { 
+            _context = context;
+            _investmentMapper = investmentMapper;
+        }
+
+        public Task<InvestmentDto> CreateInvestmentAsync(InvestmentDto investmentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteInvestmentAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<InvestmentDto>> GetAllInvestmentsAsync()
+        {
+            var entities = await _context.Investment
+                                 .AsNoTracking()
+                                 .ToListAsync();
+
+            var investmentDTOs = entities
+                .Select(e => _investmentMapper.MapToDTO(e))
+                .ToList()
+                .AsReadOnly();
+
+            return investmentDTOs;
+        }
+
+        public Task<InvestmentDto?> GetInvestmentByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<InvestmentDto?> UpdateInvestmentAsync(int id, InvestmentDto investmentDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
