7cac51068ba54fcdf5583df9cd5d12b4ca672da9
SinghM21 <manis3r3@gmail.com>
2026-01-20 21:04:24 +0000

    Added values + inputs for dashboard top row cards

--- FinancePlanner/ViewModels/DashboardViewModel.cs
+++ FinancePlanner/ViewModels/DashboardViewModel.cs
@@ -5,25 +5,32 @@ namespace FinancePlanner.ViewModels
 {
     public class DashboardViewModel
     {
-        public int currentIncome { get; set; }
-        public int currentOutcome { get; set; }
-        public decimal[]? yearlyIncome { get; set; }
-        public decimal[]? yearlyOutcome { get; set; }
+        public int CurrentIncome { get; }
+        public int CurrentOutcome { get; }
+        public int NetIncome
+        {
+            get
+            {
+                return CurrentIncome - CurrentOutcome;
+            }
+        }
+        public decimal[]? YearlyIncome { get; }
+        public decimal[]? YearlyOutcome { get; }
 
         public DashboardViewModel(int currentIncome, int currentOutcome)
         {
-            this.currentIncome = currentIncome;
-            this.currentOutcome = currentOutcome;
-            this.yearlyIncome = ProjectionCalculator.CalculateYearlyProjection(currentIncome, null);
-            this.yearlyOutcome = ProjectionCalculator.CalculateYearlyProjection(currentOutcome, null);
+            this.CurrentIncome = currentIncome;
+            this.CurrentOutcome = currentOutcome;
+            this.YearlyIncome = ProjectionCalculator.CalculateYearlyProjection(currentIncome, null);
+            this.YearlyOutcome = ProjectionCalculator.CalculateYearlyProjection(currentOutcome, null);
         }
 
         public DashboardViewModel(int currentIncome, int currentOutcome, DashboardFormValues dashboardFormValues)
         {
-            this.currentIncome = currentIncome;
-            this.currentOutcome = currentOutcome;
-            this.yearlyIncome = ProjectionCalculator.CalculateYearlyProjection(currentIncome, ProjectionCalculator.ConvertNumberToDecimal(dashboardFormValues.IncomeProjectionIncreaseNumber));
-            this.yearlyOutcome = ProjectionCalculator.CalculateYearlyProjection(currentOutcome, ProjectionCalculator.ConvertNumberToDecimal(dashboardFormValues.OutcomeProjectionIncreaseNumber));
+            this.CurrentIncome = currentIncome;
+            this.CurrentOutcome = currentOutcome;
+            this.YearlyIncome = ProjectionCalculator.CalculateYearlyProjection(currentIncome, ProjectionCalculator.ConvertNumberToDecimal(dashboardFormValues.IncomeProjectionIncreaseNumber));
+            this.YearlyOutcome = ProjectionCalculator.CalculateYearlyProjection(currentOutcome, ProjectionCalculator.ConvertNumberToDecimal(dashboardFormValues.OutcomeProjectionIncreaseNumber));
         }
     }
 }
