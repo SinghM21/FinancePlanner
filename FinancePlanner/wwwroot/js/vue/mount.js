8f32fac1da1bdfcdf110c9e5672504ee15cda366
SinghM21 <manis3r3@gmail.com>
2026-01-25 10:43:22 +0000

    Added Vue + Initial testing on mount.js with IncomesPage

---
+++ FinancePlanner/wwwroot/js/vue/mount.js
@@ -0,0 +1,19 @@
+﻿const { createApp } = Vue;
+import IncomesPage from './pages/IncomesPage.js';
+
+const mounts = {
+    'incomes-page' : IncomesPage,
+}
+
+Object.entries(mounts).forEach(([id, component]) => {
+    const element = document.getElementById(id);
+    
+    if (!element) return;
+    
+    const props = element.dataset.props
+        ? JSON.parse(element.dataset.props)
+        : {};
+    
+    createApp(component, props).mount(element);
+})
+
---
+++ FinancePlanner/wwwroot/js/vue/pages/IncomesPage.js
@@ -0,0 +1,11 @@
+﻿export default {
+    data() {
+        return {
+            message: "Hello from Vue!"
+        }
+    },
+    mounted() {
+        console.log("Vue test page mounted")
+    },
+    template: `<p>{{ message }}</p>`
+}
